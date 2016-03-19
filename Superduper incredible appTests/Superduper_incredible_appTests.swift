52eed78010607ae46a7ba9fdfcd810bf0b186800
Philippe Yu <Philippejlyu@me.com>
2016-03-18 18:45:18 -0700

    Initial Commit

---
+++ Superduper incredible appTests/Superduper_incredible_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  Superduper_incredible_appTests.swift
+//  Superduper incredible appTests
+//
+//  Created by Philippe Yu on 2016-03-18.
+//  Copyright © 2016 PhilippeYu. All rights reserved.
+//
+
+import XCTest
+@testable import Superduper_incredible_app
+
+class Superduper_incredible_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
